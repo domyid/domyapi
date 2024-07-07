fc103c38ef8e1f7dfdb40bb4c470f376f1eaf251
fahiraaa06 <fahira1678@gmail.com>
2024-07-07 22:47:36 +0700

    update doc find

--- controller/domyApi.go
+++ controller/domyApi.go
@@ -397,7 +397,7 @@ func UpdateBimbinganDisetujui(w http.ResponseWriter, r *http.Request) {
 	}
 	err := json.NewDecoder(r.Body).Decode(&requestData)
 	if err != nil || requestData.NIM == "" || requestData.Topik == "" {
-		http.Error(w, "Invalid request body or required fields not provided", http.StatusBadRequest)
+		http.Error(w, "Invalid request body or NIM/Topik not provided", http.StatusBadRequest)
 		return
 	}
 
@@ -409,7 +409,12 @@ func UpdateBimbinganDisetujui(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Memanggil fungsi helper untuk mendapatkan list tugas akhir semua mahasiswa
+	// Buat payload berisi informasi token
+	payload := map[string]string{
+		"SIAKAD_CLOUD_ACCESS": tokenData.Token,
+	}
+
+	// Mendapatkan list tugas akhir semua mahasiswa
 	listTA, err := api.FetchListTugasAkhirAllMahasiswa(tokenData.NoHp)
 	if err != nil || len(listTA) == 0 {
 		at.WriteJSON(w, http.StatusNotFound, "Failed to fetch Tugas Akhir or no data found")
@@ -429,15 +434,10 @@ func UpdateBimbinganDisetujui(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// URL untuk mendapatkan data bimbingan berdasarkan data_id
+	// URL untuk mendapatkan data bimbingan
 	urlTarget := fmt.Sprintf("https://siakad.ulbi.ac.id/siakad/list_bimbingan/%s", dataID)
 
-	// Buat payload berisi informasi token
-	payload := map[string]string{
-		"SIAKAD_CLOUD_ACCESS": tokenData.Token,
-	}
-
-	// Mengirim permintaan untuk mengambil data list bimbingan
+	// Mengirim permintaan GET untuk mendapatkan data list bimbingan
 	doc, err := api.GetData(urlTarget, payload, nil)
 	if err != nil {
 		at.WriteJSON(w, http.StatusInternalServerError, err.Error())
@@ -448,15 +448,15 @@ func UpdateBimbinganDisetujui(w http.ResponseWriter, r *http.Request) {
 	var bimbinganID string
 	doc.Find("tbody tr").Each(func(i int, s *goquery.Selection) {
 		topik := strings.TrimSpace(s.Find("td").Eq(3).Text())
-		disetujui := s.Find("td").Eq(4).Text() == ""
-		if topik == requestData.Topik && disetujui {
+		disetujui := s.Find("td").Eq(4).Text()
+		if topik == requestData.Topik && disetujui == "" {
 			bimbinganID, _ = s.Find("td").Eq(5).Find("button").Attr("data-id")
 			return
 		}
 	})
 
 	if bimbinganID == "" {
-		http.Error(w, "No valid data ID found for the provided topik", http.StatusNotFound)
+		http.Error(w, "No valid data ID found for the provided topik", http.StatusForbidden)
 		return
 	}
 
@@ -475,6 +475,7 @@ func UpdateBimbinganDisetujui(w http.ResponseWriter, r *http.Request) {
 	nip := doc.Find("input[name='nip']").AttrOr("value", "")
 	tglbimbingan := doc.Find("input[name='tglbimbingan']").AttrOr("value", "")
 	topikbimbingan := doc.Find("input[name='topikbimbingan']").AttrOr("value", "")
+	link := doc.Find("input[name='link[]']").AttrOr("value", "")
 	key := doc.Find("input[name='key']").AttrOr("value", "")
 	act := doc.Find("input[name='act']").AttrOr("value", "")
 
@@ -485,6 +486,7 @@ func UpdateBimbinganDisetujui(w http.ResponseWriter, r *http.Request) {
 	form.Add("tglbimbingan", tglbimbingan)
 	form.Add("topikbimbingan", topikbimbingan)
 	form.Add("disetujui", "1")
+	form.Add("link[]", link)
 	form.Add("key", key)
 	form.Add("act", act)
 
